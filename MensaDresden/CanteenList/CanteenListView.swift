70ded13409c7c761f395ea5543c48368f4d75478
Kilian Koeltzsch <me@kilian.io>
2019-09-30 22:06:10 +0200

    fix placeholder canteen name, oops

--- MensaDresden/CanteenList/CanteenListView.swift
+++ MensaDresden/CanteenList/CanteenListView.swift
@@ -14,7 +14,7 @@ struct CanteenListView: View {
                 coordinate: CLLocationCoordinate2D(latitude: 51.02696733929933, longitude: 13.726491630077364)),
         Canteen(id: 2, name: "Siedepunkt", address: "Zellescher Weg 17, 01069 Dresden",
                 coordinate: CLLocationCoordinate2D(latitude: 51.02946063983054, longitude: 13.738727867603302)),
-        Canteen(id: 3, name: "Mensa Reichenbergstraße", address: "Reichenbachstr. 1, 01069 Dresden",
+        Canteen(id: 3, name: "Mensa Reichenbachstraße", address: "Reichenbachstr. 1, 01069 Dresden",
                 coordinate: CLLocationCoordinate2D(latitude: 51.034283226863565, longitude: 13.734020590782166)),
     ]
 
