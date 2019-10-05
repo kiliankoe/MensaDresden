15876bfce0a0ca5363abb8ae2fbf37130adb03a8
Kilian Koeltzsch <me@kilian.io>
2019-10-05 02:25:52 +0200

    Create UI on empty Emeal card
    see #17

--- MensaDresden/EmealView.swift
+++ MensaDresden/EmealView.swift
@@ -1,15 +1,52 @@
 import SwiftUI
 
 struct EmealView: View {
+    @State var amount: Double = 13.37
+    @State var lastTransaction = 3.5
+
     var body: some View {
         VStack {
-            Image("emeal")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: UIScreen.main.bounds.width)
-                .padding()
+            ZStack(alignment: .leading) {
+                Image("emeal_empty")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: UIScreen.main.bounds.width - 40)
+                    .padding(.bottom)
+                    .shadow(radius: 10)
+
+                VStack(alignment: .leading) {
+                    Text("Balance")
+                        .font(Font.headline.smallCaps())
+                        .foregroundColor(.white)
+                    Text("\(amount, specifier: "%.2f")€")
+                        .font(.system(size: 50))
+                        .foregroundColor(.white)
+                        .padding(.bottom, UIScreen.main.bounds.height * 0.04)
+
+                    Text("Last Transaction")
+                        .font(Font.subheadline.smallCaps())
+                        .foregroundColor(.white)
+                    Text("\(lastTransaction, specifier: "%.2f")€")
+                        .font(.title)
+                        .foregroundColor(.white)
+                }.offset(x: 20, y: 0)
+            }
+
+            Button(action: {
+
+            }) {
+                Text("Scan Mensa Card")
+                    .font(.headline)
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: UIScreen.main.bounds.width - 40)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+
             Spacer()
-        }
+        }.padding()
     }
 }
 
