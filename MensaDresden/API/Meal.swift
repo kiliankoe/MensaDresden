451cd61721c0d88906ac1630d31d6fdc6c9d5750
Kilian Koeltzsch <me@kilian.io>
2019-10-11 22:25:01 +0200

    add notes to example meal

--- MensaDresden/API/Meal.swift
+++ MensaDresden/API/Meal.swift
@@ -104,7 +104,7 @@ struct Meal: Identifiable, Decodable {
     static var example: Meal {
         Meal(id: 1,
              name: "Rindfleischpfanne mit Möhre, Ananas, Mango und Kokosmilch, dazu Mie Nudeln",
-             notes: [""],
+             notes: ["enthält Rindfleisch", "Irgendwas anderes"],
              prices: Meal.Prices(students: 2.9, employees: 4.7),
              category: "Wok & Grill",
              image: URL(string: "https://bilderspeiseplan.studentenwerk-dresden.de/m18/201909/233593.jpg")!,
