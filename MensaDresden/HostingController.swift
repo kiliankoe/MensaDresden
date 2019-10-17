58b70b80376bec324a6f4be959477a738df98ba9
Kilian Koeltzsch <me@kilian.io>
2019-10-17 20:12:00 +0200

    disable landscape on iPhone

--- MensaDresden/HostingController.swift
+++ MensaDresden/HostingController.swift
@@ -1,6 +1,11 @@
+import UIKit
 import SwiftUI
 
 class HostingController<Content> : UIHostingController<Content> where Content: View {
+    override var shouldAutorotate: Bool {
+        UIDevice.current.userInterfaceIdiom == .pad
+    }
+
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         NotificationCenter.default.post(name: .md_onViewWillTransition, object: nil, userInfo: ["size": size])
