ae98f6726a5655547bcb95f7c5cc170ca771e845
Kilian Koeltzsch <me@kilian.io>
2019-11-02 12:13:55 +0100

    Fix no meal labels

--- MensaDresden/MealList/MealList.swift
+++ MensaDresden/MealList/MealList.swift
@@ -6,11 +6,28 @@ struct MealList: View {
     var canteen: Canteen
     var selectedDate: Date
 
+    var noDataMessage: LocalizedStringKey {
+        switch selectedDate {
+        case .today:
+            return "meals.no-meals-today"
+        case .tomorrow:
+            return "meals.no-meals-tomorrow"
+        default:
+            return "meals.no-meals-other"
+        }
+    }
+
+    var noDataSubtitle: LocalizedStringKey? {
+        guard selectedDate.isWeekend else { return nil }
+        guard selectedDate == .today || selectedDate == .tomorrow else { return nil }
+        return "meals.weekend"
+    }
+
     var body: some View {
         let result = store.meals(for: canteen.id, on: selectedDate)
         return LoadingListView(result: result,
-                               noDataMessage: "meals.no-meals-\(Formatter.string(forRelativeDate: selectedDate, to: .today))",
-                               noDataSubtitle: selectedDate.isWeekend ? "meals.weekend" : nil,
+                               noDataMessage: noDataMessage,
+                               noDataSubtitle: noDataSubtitle,
                                retryAction: { self.store.loadMeals(for: self.canteen.id, on: self.selectedDate) },
                                listView: { meals in
                                     List(meals) { meal in
