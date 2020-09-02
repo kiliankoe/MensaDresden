5f93817a8c367665ee651e7b00cc3ffad715ff1a
Kilian Koeltzsch <me@kilian.io>
2020-09-02 11:08:35 +0200

    Use better loading meal image indicator
    Also refactor MealImage a bit and update preview.

--- MensaDresden/MealList/MealImage.swift
+++ MensaDresden/MealList/MealImage.swift
@@ -17,35 +17,48 @@ struct MealImage: View {
             .clipShape(RoundedRectangle(cornerRadius: self.roundedCorners ? 8 : 0))
     }
 
+    var loadingImage: some View {
+        ZStack {
+            Image("meal_sample")
+                .resizable()
+                .aspectRatio(contentMode: contentMode)
+                .blur(radius: 5, opaque: true)
+                .opacity(0.5)
+                .frame(width: width, height: height)
+                .clipShape(RoundedRectangle(cornerRadius: self.roundedCorners ? 8 : 0))
+            ActivityIndicator()
+        }
+    }
+
+    func mealImage(_ image: Image) -> some View {
+        image
+            .resizable()
+            .aspectRatio(contentMode: self.contentMode)
+            .frame(width: self.width, height: self.height)
+            .clipShape(RoundedRectangle(
+                        cornerRadius: self.roundedCorners ? 8 : 0))
+    }
+
     var body: some View {
         if imageURL == Meal.placeholderImageURL {
-            return AnyView(placeholderImage)
-        }
-
-        return AnyView(
+            placeholderImage
+        } else {
             RemoteImage(type: .url(imageURL),
-                        errorView: { _ in self.placeholderImage },
-                        imageView: { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: self.contentMode)
-                                .frame(width: self.width, height: self.height)
-                                .clipShape(RoundedRectangle(cornerRadius: self.roundedCorners ? 8 : 0))
-                        },
-                        loadingView: {
-                            self.placeholderImage
-                                .opacity(0.5)
-                        })
-        )
+                        errorView: { _ in placeholderImage },
+                        imageView: mealImage,
+                        loadingView: { loadingImage })
+        }
     }
 }
 
 struct MealImage_Previews: PreviewProvider {
     static var previews: some View {
-        MealImage(imageURL: URL(string: "https://bilderspeiseplan.studentenwerk-dresden.de/m18/201909/233593.jpg")!,
-                  width: 130,
-                  height: 95,
+        MealImage(imageURL: URL(string: "https://bilderspeiseplan.studentenwerk-dresden.de/m15/202008/247506.jpg")!,
+                  width: 400,
+                  height: 300,
                   roundedCorners: true,
                   contentMode: .fill)
+            .padding()
+            .previewLayout(.sizeThatFits)
     }
 }
