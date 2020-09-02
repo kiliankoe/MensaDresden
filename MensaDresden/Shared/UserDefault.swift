77237e2c099d7b7c50f7e215b38e57e63c82ee0c
Kilian Koeltzsch <me@kilian.io>
2020-09-02 11:44:09 +0200

    Add custom suite to UserDefault property wrapper

--- MensaDresden/Shared/UserDefault.swift
+++ MensaDresden/Shared/UserDefault.swift
@@ -1,21 +1,24 @@
 import Foundation
 
+#warning("Delete me and use `AppStorage` when dropping iOS 13.")
 @propertyWrapper
 struct UserDefault<T> {
     let key: String
     let defaultValue: T
+    let suite: UserDefaults
 
-    init(_ key: String, defaultValue: T) {
+    init(_ key: String, defaultValue: T, suite: UserDefaults = .standard) {
         self.key = key
         self.defaultValue = defaultValue
+        self.suite = suite
     }
 
     var wrappedValue: T {
         get {
-            UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+            suite.object(forKey: key) as? T ?? defaultValue
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: key)
+            suite.set(newValue, forKey: key)
         }
     }
 }
