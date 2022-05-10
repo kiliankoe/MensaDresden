bc85af6f53710294dcac7eee4cb60972f6704de0
Kilian Koeltzsch <me@kilian.io>
2022-05-10 14:04:25 +0200

    Estimate Emeal balance based on latest transactions
    closes #41

--- MensaDresden/EmealView/EmealView.swift
+++ MensaDresden/EmealView/EmealView.swift
@@ -34,14 +34,33 @@ struct EmealView: View {
         #endif
     }
 
+    // If there are autoload transactions newer than the last scan, we'll
+    // use those to calculate an estimated current balance.
+    var emealBalance: Double {
+        guard let lastScanDate = emeal.lastScanDate,
+              case .success(let transactions) = api.transactions()
+        else {
+            return emeal.currentBalance
+        }
+
+        let newerTransactionsSum = transactions
+            .filter { $0.date > lastScanDate }
+            .reduce(0) { $0 + $1.amount }
+        
+        return emeal.currentBalance - newerTransactionsSum
+    }
+
     var body: some View {
         NavigationView {
             VStack {
                 if shouldShowEmealView {
-                    EmealCardView(amount: $emeal.currentBalance,
-                                  lastTransaction: $emeal.lastTransaction,
-                                  lastScan: $emeal.lastScanDate)
-                        .padding(.horizontal)
+                    EmealCardView(
+                        amount: self.emealBalance,
+                        actualAmount: emeal.currentBalance,
+                        lastTransaction: emeal.lastTransaction,
+                        lastScan: emeal.lastScanDate
+                    )
+                    .padding(.horizontal)
 
                     LargeButton(content: {
                         Text("emeal.scan-button")
@@ -52,37 +71,38 @@ struct EmealView: View {
                 }
 
                 if settings.areAutoloadCredentialsAvailable {
-                    LoadingListView(result: api.transactions(),
-                                    noDataMessage: "emeal.no-transactions",
-                                    retryAction: {
-                                        Task { await self.loadTransactions() }
-                                    },
-                                    listView: { transactions in
-                                        List(transactions, id: \.id) { transaction in
-                                            VStack(alignment: .leading) {
-                                                Text(Formatter.string(for: transaction.date, dateStyle: .medium, timeStyle: .short))
-                                                    .font(.caption)
-                                                    .foregroundColor(.gray)
-                                                HStack {
-                                                    Text(transaction.location)
-                                                    Spacer()
-                                                    Text("\(transaction.amount, specifier: "%.2f")€")
-                                                }
-                                                ForEach(transaction.positions, id: \.id) { pos in
-                                                    HStack {
-                                                        Text(pos.name)
-                                                        Spacer()
-                                                        Text("\(pos.price, specifier: "%.2f")€")
-                                                    }
-                                                    .font(.caption)
-                                                }
-                                            }
-                                        }
-                                        .listStyle(PlainListStyle())
-                                        .refreshable {
-                                            await self.loadTransactions()
-                                        }
+                    LoadingListView(
+                        result: api.transactions(),
+                        noDataMessage: "emeal.no-transactions",
+                        retryAction: {
+                            Task { await self.loadTransactions() }
+                        },
+                        listView: { transactions in
+                            List(transactions, id: \.id) { transaction in
+                                VStack(alignment: .leading) {
+                                    Text(Formatter.string(for: transaction.date, dateStyle: .medium, timeStyle: .short))
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                    HStack {
+                                        Text(transaction.location)
+                                        Spacer()
+                                        Text("\(transaction.amount, specifier: "%.2f")€")
                                     }
+                                    ForEach(transaction.positions, id: \.id) { pos in
+                                        HStack {
+                                            Text(pos.name)
+                                            Spacer()
+                                            Text("\(pos.price, specifier: "%.2f")€")
+                                        }
+                                        .font(.caption)
+                                    }
+                                }
+                            }
+                            .listStyle(PlainListStyle())
+                            .refreshable {
+                                await self.loadTransactions()
+                            }
+                        }
                     )
                 } else {
                     autoloadHint
