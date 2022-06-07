d9643cdde324e5552cf50743047d86a6a53daf8f
Kilian Koeltzsch <me@kilian.io>
2022-06-07 22:47:25 +0200

    Remove explicit regex type and usage
    Didn't use this anyways, that's unnecessary

--- MensaDresden/Extensions/Meal.swift
+++ MensaDresden/Extensions/Meal.swift
@@ -1,12 +1,7 @@
 import Foundation
 import EmealKit
-import Regex
 
 extension Meal {
-    private static var allergenRegex: Regex {
-        Regex(#" \([A-N]\d?(?:, )?\)"#)
-    }
-
     var allergenStrippedTitle: String {
         self.name.replacingOccurrences(of: #" (\((?:[A-N]\d?(?:, )?)+\))"#, with: "", options: .regularExpression)
     }
